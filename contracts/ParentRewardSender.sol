85e5e2c002e42ba9719056dd584064e364838373
codebatine <codebatine@proton.me>
2025-06-09 10:19:42 +0200

    parent contract update

--- contracts/ParentRewardSender.sol
+++ contracts/ParentRewardSender.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import "@chainlink/contracts-ccip/src/v0.8/CCIPSender.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "https://raw.githubusercontent.com/smartcontractkit/chainlink-ccip/develop/chains/evm/contracts/applications/CCIPSender.sol";
+import "https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-contracts/release-v4.9/contracts/access/Ownable.sol";
+import "https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-contracts/release-v4.9/contracts/token/ERC20/IERC20.sol";
 
 contract ParentRewardSender is CCIPSender, Ownable {
     address public rewardToken;
@@ -19,7 +19,7 @@ contract ParentRewardSender is CCIPSender, Ownable {
     function sendReward(address child, uint256 amount, uint64 destinationChainSelector) external onlyOwner {
         require(IERC20(rewardToken).balanceOf(address(this)) >= amount, "Insufficient balance");
         bytes memory message = abi.encode(child, amount);
-        bytes32 messageId = _send(destinationChainSelector, message, LINK_TOKEN, 0);
+        bytes32 messageId = _send(destinationChainSelector, message, rewardToken, amount);
         emit RewardSent(messageId, child, amount, destinationChainSelector);
     }
 
@@ -28,6 +28,6 @@ contract ParentRewardSender is CCIPSender, Ownable {
     }
 
     function withdrawTokens(address token, address to, uint256 amount) external onlyOwner {
-        IERC20(token).transfer(to, amount);
+        require(IERC20(token).transfer(to, amount), "Withdraw failed");
     }
 }
